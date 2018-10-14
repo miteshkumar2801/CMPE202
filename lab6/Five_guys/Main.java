6ca268937853f812f9c4c3c90028d3aa38ad316b
Mitesh Kumar <mitesh@internal.synopsys.com>
2018-10-13 19:45:08 -0700

    lab6 commit1

---
+++ lab6/Five_guys/Main.java
@@ -0,0 +1,19 @@
+final class Main {
+
+    private Main() {
+        // Utility Class
+        return;
+    }
+
+    /**
+     * Main App Entry Point.
+     *
+     * @param args No args expected.
+     */
+    public static void main(final String[] args) {
+        // calculate final Bill
+        System.out.println("Calculating Total Cost and Printing Reciept");
+
+    }
+
+}
